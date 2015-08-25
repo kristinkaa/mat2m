ca37e84ddc8350c388ce67c3e5c843d6cab0aff3
Jonas Mueller <jonas@muellers.one>
2015-08-25 15:04:21 +0200

    Pimp integration spec

--- spec/edi_spec.m
+++ spec/edi_spec.m
@@ -3,3 +3,14 @@ function edi_spec
 % Convert *.mat file
 mat2m([projectpath '/spec/fixtures/TESTFILE_MAT.mat'], ...
   [projectpath '/tmp/TESTFILE_M.m']);
+
+% Load the resulting *.m file
+run([projectpath '/tmp/TESTFILE_M.m']);
+
+expect(exist('Cruise', 'var'));
+expect(exist('ECU', 'var'));
+expect(exist('Settings', 'var'));
+
+expect(isstruct(Cruise));
+expect(isstruct(ECU));
+expect(isstruct(Settings));
